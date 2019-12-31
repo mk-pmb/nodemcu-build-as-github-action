571988ff0c29ac7487c7169ac5d64fa23b793887
mk-pmb <mk-pmb@users.noreply.github.com>
2019-12-31 05:02:48 +0100

    Move script to subdir

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # -*- coding: utf-8, tab-width: 2 -*-
 
 FROM marcelstoer/nodemcu-build:latest
-COPY build.sh /nodemcu-build-as-github-action.sh
-ENTRYPOINT ["/nodemcu-build-as-github-action.sh"]
+COPY ./build-gha/ /opt/build-gha/
+ENTRYPOINT ["/opt/build-gha/build.sh"]
--- build-gha/build.sh
+++ build-gha/build.sh
@@ -4,22 +4,11 @@
 
 function build () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
-
   local LS='ls --file-type --human-readable --group-directories-first'
-  LS+=' --format=long'
-  echo "args: $*"
-
-  pwd
-  $LS
-
   local SELFPATH="$(readlink -m "$BASH_SOURCE"/..)"
   cd -- "$SELFPATH" || return $?
 
-  pwd
-  $LS . /opt/{,*/}
-
-  echo '== env =='
-  env | grep -Pe '^\S' | sort --version-sort
+  $LS "$RUNNER_WORKSPACE"
 }
 
 
