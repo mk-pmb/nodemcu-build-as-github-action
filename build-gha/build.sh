08d8b2dce56c8cff58592467ebfd6ee9d2ecbba5
mk-pmb <mk-pmb@users.noreply.github.com>
2019-12-31 05:32:05 +0100

    Clone the firmware

--- build-gha/build.sh
+++ build-gha/build.sh
@@ -6,9 +6,30 @@ function build () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
   local LS='ls --file-type --human-readable --group-directories-first'
   local SELFPATH="$(readlink -m "$BASH_SOURCE"/..)"
-  cd -- "$SELFPATH" || return $?
+  # cd -- "$SELFPATH" || return $?
 
-  $LS "$RUNNER_WORKSPACE"
+  exec &> >( # try to work around an annoying CORS issue on GHA 2019-12-31
+    sed -re 's~^~::warning file=build.log,line=0,col=0::~')
+
+  local FWSRC_DIR='/opt/nodemcu-firmware'
+  git clone --recurse-submodules \
+    --single-branch --branch "$INPUT_FIRMWARE_BRANCH" \
+    "$INPUT_FIRMWARE_REPO" "$FWSRC_DIR" || return $?
+  cd -- "$FWSRC_DIR" || return $?
+
+  local ITEM=
+  for ITEM in app/include/user_*.h; do
+    snipdump_file "$ITEM"
+  done
+}
+
+
+function snipdump_file () {
+  echo
+  echo "----- 8< --== $1 ==-- 8< ----- 8< ----- 8< ----- 8< ----- 8< -----"
+  nl -ba -- "$1"
+  echo "----- >8 --== $1 ==-- >8 ----- >8 ----- >8 ----- >8 ----- >8 -----"
+  echo
 }
 
 
