2cf13ae3472376cfb7729f270b408ee608ea64db
mk-pmb <mk-pmb@users.noreply.github.com>
2020-01-03 07:30:50 +0100

    Support ESP32 SDK config

--- build.sh
+++ build.sh
@@ -23,44 +23,107 @@ function build_main () {
   fi
   cd -- "$INPUT_FIRMWARE_SRCDIR" || return $?
   [ -z "$INPUT_FIRMWARE_GIT_RESET" ] \
-    || snip_run git reset --hard "$INPUT_FIRMWARE_GIT_RESET" || return $?
+    || snip_run '' git reset --hard "$INPUT_FIRMWARE_GIT_RESET" || return $?
   snip_run '' git submodule update --recursive || return $?
 
-  echo -n 'Firmware repo is at commit: ';
+  echo -n 'D: Firmware repo is at commit: ';
   git log --format=oneline --abbrev-commit --max-count=1
+  local MCU_PLATFORM="$(guess_mcu_platform)"
+  echo "D: target platform was guessed as: ${MCU_PLATFORM:-?? unknown ??}"
+  [ -n "$MCU_PLATFORM" ] || return 3
 
-  snip_ls "$INPUT_FIRMWARE_SRCDIR"/bin/
-  snip_ls /opt/lua/
-
+  debug_ls_relevant_dirs
   build_core
   local CORE_RV=$?
   echo "##### core rv=$CORE_RV #####"
 
-  snip_ls "$INPUT_FIRMWARE_SRCDIR"/bin/
-  snip_ls /opt/lua/
-  snip_ls "$FWDEST_DIR"
-
+  debug_ls_relevant_dirs
   return "$CORE_RV"
 }
 
 
+function guess_mcu_platform () {
+  local PLAT="$(printf '%s\n' \
+    [a-z]*/platform/{include/,}cpu_*.h \
+    | sed -nre '
+      s~\.h$~~
+      /\*$/d
+      s~^.*/cpu_~~p
+    ')"
+  case "$PLAT" in
+    '' )
+      echo "E: $FUNCNAME: found no candidates at all." >&2
+      return 7;;
+    *$'\n'* )
+      echo "E: $FUNCNAME: found too many candidates: ${PLAT//$'\n'/, }" >&2
+      return 8;;
+  esac
+  echo "$PLAT"
+}
+
+
+function debug_ls_relevant_dirs () {
+  local FWSRC="$INPUT_FIRMWARE_SRCDIR"
+  snip_ls "$FWSRC"/bin/
+  snip_ls "$FWSRC"/sdk/esp32-esp-idf/
+  snip_ls /opt/lua/
+}
+
+
 function build_core () {
-  copy_custom_user_headers || return $?
+  case "$MCU_PLATFORM" in
+    esp32 )
+      "$MCU_PLATFORM"_prepare_toolchain || return $?;;
+  esac
+
+  snip_run '' "$MCU_PLATFORM"_copy_custom_config || return $?
   IMAGE_NAME='IMAGE_NAME' snip_run '' /opt/build || return $?
   move_output_files || return $?
 }
 
 
-function copy_custom_user_headers () {
+function esp32_prepare_toolchain () {
+  cp --no-target-directory -- sdkconfig{.defaults,} || return $?
+  snip_run '' /opt/configure-esp32
+  local CFG_RV=$?
+  snip_ls "$FWSRC"/cache/
+  snip_run 'toolchain checksums' sha1sum --binary cache/toolchain*.*z
+  return "$CFG_RV"
+}
+
+
+function esp8266_copy_custom_config () {
   local SRC= BFN= DEST=
-  for SRC in /github/workspace/app.include/*.h; do
+  for SRC in "$REPO_DIR/$MCU_PLATFORM.app.include"/*.h; do
     [ -f "$SRC" ] || continue
     BFN="$(basename -- "$SRC")"
     DEST="$INPUT_FIRMWARE_SRCDIR/app/include/user_$BFN"
     cp --verbose --no-target-directory -- "$SRC" "$DEST" || return $?
   done
   snip_run 'user config MD5s' md5sum --binary \
-    -- "$INPUT_FIRMWARE_SRCDIR"/app/include/user_* || true
+    -- "$INPUT_FIRMWARE_SRCDIR"/app/include/user_* || return $?
+}
+
+
+function esp32_copy_custom_config () {
+  local DEST="$INPUT_FIRMWARE_SRCDIR/sdkconfig"
+  if [ -f "$DEST" ]; then
+    echo "D: $FUNCNAME: skip: target exists: $DEST"
+    return 0
+  fi
+
+  local RC_DIR="$REPO_DIR/$MCU_PLATFORM.sdkcfg/"
+  local SRC= SUB=
+  for SRC in "$RC_DIR"[0-9]{,*/}*.rc; do
+    [ -f "$SRC" ] || continue
+    SUB="${SRC#$RC_DIR}"
+    echo
+    echo "##### >> $SUB >> #####"
+    cat -- "$SRC" || return $?
+    echo
+    echo "##### << $SUB << #####"
+    echo
+  done >>"$DEST" || return $?
 }
 
 
